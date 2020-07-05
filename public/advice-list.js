526ac5dcab5f7ab03e07b9457557a8f031efdb20
safv12 <ing.fran12@gmail.com>
2020-07-04 19:33:11 -0500

    Improve advice randomicity

--- public/advice-list.js
+++ public/advice-list.js
@@ -1,109 +1,63 @@
-export default
-[
-  {
-    "topic": "Domain Driven Design",
-    "advices": [
-      "Protect business invariants inside aggregate boundaries.",
-      "Collaborate with the business experts.",
-      "The bounded context is the region where an official language is spoken.",
-      "To achieve a good design, we need to collaborate with the domain experts.",
-      "It’s important to identify bounded contexts before grouping the models.",
-      "You must maintain an ubiquitous language.",
-      "Ubiquitous Language is the term of building up a common language between developers and users.",
-      "A DDD aggregate is a cluster of domain objects that can be treated as a single unit.",
-      "An aggregate will have one of its component objects be the aggregate root.",
-      "The aggregate root must ensure the integrity of the aggregate as a whole.",
-      "Use domain events to explicitly implement side effects across multiple aggregates.",
-      "Anti Corruption Layer is used when the downstream model don’t want to be affected by an upstream model.",
-      "Aggregate rule of thumb: Protect business invariants inside aggregate boundaries.",
-      "Aggregate rule of thumb: Design small aggregates.",
-      "Aggregate rule of thumb: Reference other aggregates by identity only.",
-      "Aggregate rule of thumb: Update other aggregates using eventual consistency.",
-      "Avoid the anemic domain model.",
-      "An anemic domain model is a void of behavior."
-    ]
-  },
-  {
-    "topic": "Production ready applications",
-    "advices": [
-      "Automate your software testing.",
-      "Configure a CI/CD pipeline.",
-      "Logging is a fundamental part of the applications.",
-      "A well-designed logging system is a great utility for system administrators and developers.",
-      "Logs save many valuable hours for both the support team or developers.",
-      "Continuous Integration establishes an automated way to build, package, and test applications.",
-      "You should configure a good application monitoring.",
-      "Application Performance Monitoring (APM).",
-      "Blue/Green deployments could be used to minimize the impact of deployments into production.",
-      "Canary deployments could be used to minimize the impact of deployments into production.",
-      "Feature Flagging could be used to minimize the impact of deployments into production.",
-      "Dark Launches could be used to minimize the impact of deployments into production.",
-    ]
-  },
-  {
-    "topic": "CQRS",
-    "advices": [
-      "CQRS is an architectural pattern that separates the models for reading and writing data.",
-      "Queries return a result and do not change the state of the system.",
-      "Queries are free of side effects.",
-      "Commands can change the state of the system.",
-      "Since commands are imperatives, they are typically named with a verb in the imperative mood.",
-      "Commands are immutable. They do not need to change during their projected lifetime.",
-      "You should implement a specific command handler class for each command.",
-      "Typically, a command handler deals with a single aggregate driven by its aggregate root.",
-      "When command handlers get complex, with too much logic, that can be a code smell.",
-    ]
-  },
-  {
-    "topic": "Distributed Systems Observability",
-    "advices": [
-      "The goal of observability it's to build a culture of engineering based on facts and feedback.",
-      "Logs, metrics, and traces are often known as the three pillars of observability.",
-      "Testing is a best-effort verification of the correctness of a system.",
-      "Testing is a best-effort simulation of failure modes.",
-      "Coding for failure entails acknowledging that systems will fail.",
-      "Failure needs to be embraced at every phase, from system design to implementation, testing, deployment, and, finally, operation.",
-    ]
-  },
-  {
-    "topic": "Designing Data Intensive Applications",
-    "advices": [
-      "Reliability: Continuing to work correctly, even when things go wrong.",
-      "Systems that anticipate faults and cope with them are called fault-tolerant or resilient.",
-      "Allow quick an easy recovery from human errors, to minimize the impact in the case of a failure.",
-      "Monitoring can show us ealy warning signals.",
-      "When a problem occurs, metrics can be invaluable in diagnosing the issue.",
-      "Scalability is the term used to describe a system's ability to cope with incrased load.",
-      "Reliability means making sytems work correctly, even when faults occur.",
-      "Scalability means having strategies for keeping performance good, even when load incrases.",
-      "Anything that is meaningful to humans may need to change in the future.",
-    ]
-  },
-  {
-    "topic": "Algorithms and Data Structures",
-    "advices": [
-      "Big O notation allows us to analyze and improve the efficiency of our algorithms.",
-    ]
-  },
-  {
-    "topic": "Chaos Engineering",
-    "advices": [
-      "Failure is inevitable, disaster not."
-    ]
-  },
-  {
-    "topic": "Reviewing Code",
-    "advices": [
-      "Pull request needs to do just one thing.",
-      "Automate as many checks as possible.",
-    ]
-  },
-  {
-    "topic": "Mindfulness",
-    "advices": [
-      "Two skills define a mindful mind: focus and awareness.",
-      "Focus is the ability to concentrate on what you’re doing in the moment.",
-      "Awareness is the ability to recognize and release unnecessary distractions as they arise."
-    ]
-  }
+export default [
+  ["Algorithms and Data Structures", "Big O notation allows us to analyze and improve the efficiency of our algorithms."],
+  ["Chaos Engineering", "Failure is inevitable, disaster not."],
+  ["CQRS", "Commands are immutable. They do not need to change during their projected lifetime."],
+  ["CQRS", "Commands can change the state of the system."],
+  ["CQRS", "CQRS is an architectural pattern that separates the models for reading and writing data."],
+  ["CQRS", "Queries are free of side effects."],
+  ["CQRS", "Queries return a result and do not change the state of the system."],
+  ["CQRS", "Since commands are imperatives, they are typically named with a verb in the imperative mood."],
+  ["CQRS", "Typically, a command handler deals with a single aggregate driven by its aggregate root."],
+  ["CQRS", "When command handlers get complex, with too much logic, that can be a code smell."],
+  ["CQRS", "You should implement a specific command handler class for each command."],
+  ["Designing Data Intensive Applications", "Allow quick an easy recovery from human errors, to minimize the impact in the case of a failure."],
+  ["Designing Data Intensive Applications", "Anything that is meaningful to humans may need to change in the future."],
+  ["Designing Data Intensive Applications", "Monitoring can show us ealy warning signals."],
+  ["Designing Data Intensive Applications", "Reliability means making sytems work correctly, even when faults occur."],
+  ["Designing Data Intensive Applications", "Reliability: Continuing to work correctly, even when things go wrong."],
+  ["Designing Data Intensive Applications", "Scalability is the term used to describe a system's ability to cope with incrased load."],
+  ["Designing Data Intensive Applications", "Scalability means having strategies for keeping performance good, even when load incrases."],
+  ["Designing Data Intensive Applications", "Systems that anticipate faults and cope with them are called fault-tolerant or resilient."],
+  ["Designing Data Intensive Applications", "When a problem occurs, metrics can be invaluable in diagnosing the issue."],
+  ["Distributed Systems Observability", "Coding for failure entails acknowledging that systems will fail."],
+  ["Distributed Systems Observability", "Failure needs to be embraced at every phase, from system design to implementation, testing, deployment, and, finally, operation."],
+  ["Distributed Systems Observability", "Logs, metrics, and traces are often known as the three pillars of observability."],
+  ["Distributed Systems Observability", "Testing is a best-effort simulation of failure modes."],
+  ["Distributed Systems Observability", "Testing is a best-effort verification of the correctness of a system."],
+  ["Distributed Systems Observability", "The goal of observability it's to build a culture of engineering based on facts and feedback."],
+  ["Domain Driven Design", "A DDD aggregate is a cluster of domain objects that can be treated as a single unit."],
+  ["Domain Driven Design", "Aggregate rule of thumb: Design small aggregates."],
+  ["Domain Driven Design", "Aggregate rule of thumb: Protect business invariants inside aggregate boundaries."],
+  ["Domain Driven Design", "Aggregate rule of thumb: Reference other aggregates by identity only."],
+  ["Domain Driven Design", "Aggregate rule of thumb: Update other aggregates using eventual consistency."],
+  ["Domain Driven Design", "An aggregate will have one of its component objects be the aggregate root."],
+  ["Domain Driven Design", "An anemic domain model is a void of behavior."],
+  ["Domain Driven Design", "Anti Corruption Layer is used when the downstream model don’t want to be affected by an upstream model."],
+  ["Domain Driven Design", "Avoid the anemic domain model."],
+  ["Domain Driven Design", "Collaborate with the business experts."],
+  ["Domain Driven Design", "It’s important to identify bounded contexts before grouping the models."],
+  ["Domain Driven Design", "Protect business invariants inside aggregate boundaries."],
+  ["Domain Driven Design", "The aggregate root must ensure the integrity of the aggregate as a whole."],
+  ["Domain Driven Design", "The bounded context is the region where an official language is spoken."],
+  ["Domain Driven Design", "To achieve a good design, we need to collaborate with the domain experts."],
+  ["Domain Driven Design", "Ubiquitous Language is the term of building up a common language between developers and users."],
+  ["Domain Driven Design", "Use domain events to explicitly implement side effects across multiple aggregates."],
+  ["Domain Driven Design", "You must maintain an ubiquitous language."],
+  ["Mindfulness", "Awareness is the ability to recognize and release unnecessary distractions as they arise."],
+  ["Mindfulness", "Focus is the ability to concentrate on what you’re doing in the moment."],
+  ["Mindfulness", "Two skills define a mindful mind: focus and awareness."],
+  ["Production Ready Applications", "A well-designed logging system is a great utility for system administrators and developers."],
+  ["Production Ready Applications", "Application Performance Monitoring (APM)."],
+  ["Production Ready Applications", "Automate your software testing."],
+  ["Production Ready Applications", "Blue/Green deployments could be used to minimize the impact of deployments into production."],
+  ["Production Ready Applications", "Canary deployments could be used to minimize the impact of deployments into production."],
+  ["Production Ready Applications", "Configure a CI/CD pipeline."],
+  ["Production Ready Applications", "Continuous Integration establishes an automated way to build, package, and test applications."],
+  ["Production Ready Applications", "Dark Launches could be used to minimize the impact of deployments into production."],
+  ["Production Ready Applications", "Feature Flagging could be used to minimize the impact of deployments into production."],
+  ["Production Ready Applications", "Logging is a fundamental part of the applications."],
+  ["Production Ready Applications", "Logs save many valuable hours for both the support team or developers."],
+  ["Production Ready Applications", "You should configure a good application monitoring."],
+  ["Reviewing Code", "Automate as many checks as possible."],
+  ["Reviewing Code", "Pull request needs to do just one thing."],
 ]
--- public/advice.js
+++ public/advice.js
@@ -1,11 +1,8 @@
 import advices from "./advice-list.js";
 
 (function newAdvice() {
-  var randomTopic = Math.floor(Math.random() * advices.length);
-  var topic = advices[randomTopic];
-  var randomAdvice = Math.floor(Math.random() * topic.advices.length);
-  var advice = topic.advices[randomAdvice];
-
-  document.getElementById('advice-display').innerHTML = advice;
-  document.getElementById('advice-topic-display').innerHTML = topic.topic;
+  var randomAdvice = Math.floor(Math.random() * advices.length);
+  var advice = advices[randomAdvice];
+  document.getElementById('advice-display').innerHTML = advice[1];
+  document.getElementById('advice-topic-display').innerHTML = advice[0];
 })();
